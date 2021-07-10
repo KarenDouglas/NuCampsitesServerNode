02db40df8cd6e538cbf3390063241ca6b7c3f831
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2021-07-10 14:22:33 -0500

    week 4 assignment

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '491963741906848',
+        clientSecret: 'a9705912b2c45e7f01fee81184426d8d'
+    }
 }
